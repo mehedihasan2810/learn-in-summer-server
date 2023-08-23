5672e9d9d5aaf60ad1fa87afe73648065d6782d5
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-08-23 20:10:33 +0600

    added new field to payments schema

--- dist/models/payments.model.js
+++ dist/models/payments.model.js
@@ -5,10 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const PaymentsSchema = new mongoose_1.default.Schema({
+    class_name: { type: String, require: true },
+    instructor_name: { type: String, require: true },
+    student_name: { type: String, require: true },
     student_email: { type: String, require: true },
     instructor_email: { type: String, require: true },
     transactionId: { type: String, require: true },
-    price: { type: String, require: true },
+    price: { type: Number, require: true },
     classId: { type: String, require: true },
     status: { type: String, require: true },
     date: { type: Number, require: true },
--- src/models/payments.model.ts
+++ src/models/payments.model.ts
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
 const PaymentsSchema = new mongoose.Schema({
+  class_name: {type: String, require: true},
+  instructor_name: {type: String, require: true},
+  student_name: {type: String, require: true},
   student_email: { type: String, require: true },
   instructor_email: { type: String, require: true },
   transactionId: { type: String, require: true },
-  price: { type: String, require: true },
+  price: { type: Number, require: true },
   classId: { type: String, require: true },
   status: { type: String, require: true },
   date: { type: Number, require: true },
